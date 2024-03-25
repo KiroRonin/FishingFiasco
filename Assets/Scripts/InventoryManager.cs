30ca5cc4e6819b73ae814895d304e3f7631f1976
luchaaa <lgortana@pratt.edu>
2024-03-24 21:18:49 -0400

    adding inventory(DOESNT WORK)

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    
+    public GameObject InventoryMenu;
+    private bool menuActivated;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        //code for activating and deactivating menu - STILL PENDING W. CONTROLS
+
+        if ([INSERT INPUT FOR TAB] && menuActivated){
+            InventoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+        else if ([INSERT INPUT FOR TAB] && !menuActivated){
+            InventoryMenu.SetActive(true);
+            menuActivated = true;
+        }
+
+        */
+    }
+}
