7310db0530bcc6df2b76ef1474b2e419058f9325
luchaaa <lgortana@pratt.edu>
2024-03-26 22:45:52 -0400

    adding fish fbx.... its amazing...

---
+++ Assets/ScriptableObjects/FishScriptableObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Fish Scriptable Object", menuName = "ScriptableObjects/Fish")]
+public class FishScriptableObject : ScriptableObject
+{
+    public string fishName = "fishy";
+    public int size = 10;
+    public int cost = 5;
+
+    
+
+}
---
+++ Assets/Scripts/FishRotation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishRotation : MonoBehaviour
+{
+    public float rotationSpeed = 25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.down * rotationSpeed * Time.deltaTime);
+    }
+}
