5d935ffb51071be6b14e8ddbe5d104a24876d410
luchaaa <lgortana@pratt.edu>
2024-04-27 21:16:23 -0400

    fixed tutorial entering + teleport

---
+++ Assets/Scripts/TutorialMethods/ColliderBehavior.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderBehavior : MonoBehaviour
+{
+    public TeachMinigame tutorialScript;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player")
+        {
+            print("minigame tutorial start");
+            tutorialScript.startTutorial();
+            tutorialScript.colliderEnter = true;
+
+        }
+    }
+}
--- Assets/Scripts/TutorialMethods/TeachMinigame.cs
+++ Assets/Scripts/TutorialMethods/TeachMinigame.cs
@@ -13,42 +13,41 @@ public class TeachMinigame : MonoBehaviour
     public Transform target;
     public float rotationSpeed;
 
-    private bool triggered;
+    public GameObject tutorialCollider;
 
     public GameObject fishTutorialPosition;
 
+    public bool colliderEnter;
 
     void Start()
     {
-
+        
     }
 
     void Update()
     {
-        if (triggered == true && DiaManager.instance.canvasActivated == true)
+        if (colliderEnter == true && DiaManager.instance.canvasActivated == true)
         {
             rotateBilly();
             Player.enabled = false;
-        }
-
-        if (triggered == true && DiaManager.instance.canvasActivated == false)
-        {
-            PlayerObject.transform.rotation = fishTutorialPosition.transform.rotation;
-            PlayerObject.transform.position = fishTutorialPosition.transform.position;
             
-            Destroy(this.gameObject);
         }
-    }
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "Player")
+        if (colliderEnter == true && DiaManager.instance.canvasActivated == false)
         {
-            print("minigame tutorial start");
-            startTutorial();
+
+            PlayerObject.transform.rotation = fishTutorialPosition.transform.rotation;
+            print("current pos: " + PlayerObject.transform.position);
+            PlayerObject.transform.position = fishTutorialPosition.transform.position;
+            print("player teleported from " + fishTutorialPosition.transform.position + "to " + PlayerObject.transform.position);
+            
+            Destroy(tutorialCollider.gameObject);
+            colliderEnter = false;
+            Player.enabled = true;
         }
     }
 
-    void startTutorial()
+    public void startTutorial()
     {
         var tutorial = "BillyTutorial";
         var knot = DiaManager.instance.currentKnot;
@@ -61,14 +60,13 @@ public class TeachMinigame : MonoBehaviour
         DiaManager.instance.dialogueText.text = DiaManager.instance.currentText;
         DiaManager.instance.dialogueCanvas.SetActive(true);
 
-        triggered = true;
     }
 
     private void rotateBilly()
     {
-        UnityEngine.Vector3 dir = target.position - PlayerObject.transform.position;
+        Vector3 dir = target.position - PlayerObject.transform.position;
 
-        UnityEngine.Quaternion rotation = UnityEngine.Quaternion.Slerp(PlayerObject.transform.rotation, UnityEngine.Quaternion.LookRotation(dir), rotationSpeed * Time.deltaTime);
+        Quaternion rotation = Quaternion.Slerp(PlayerObject.transform.rotation, Quaternion.LookRotation(dir), rotationSpeed * Time.deltaTime);
 
         rotation.x = 0;
         rotation.z = 0;
